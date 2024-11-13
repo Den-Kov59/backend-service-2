deec9acc0c69990b8793de632430df835e1c3983
Denis Kovalenko <den.kov.2015@gmail.com>
2024-11-13 12:31:46 +0200

    add workflow

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
-import express from "express";
-const app = express();
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+/* eslint-disable @typescript-eslint/no-require-imports */
+const dgram = require('dgram');
+const admin = require('firebase-admin');
+
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const server = dgram.createSocket('udp4');
+
+server.on('message', (msg, rinfo) => {
+  console.log(`Received message: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  
+  // Handle FCM notification
+  const message = {
+    notification: {
+      title: 'New Update',
+      body: msg.toString(),
+    },
+    topic: 'Ttopic',
+  };
+
+  admin.messaging().send(message)
+    .then(response => {
+      console.log('Successfully sent message:', response);
+    })
+    .catch(error => {
+      console.log('Error sending message:', error);
+    });
 });
+
+server.bind(process.env.port || 8080);
